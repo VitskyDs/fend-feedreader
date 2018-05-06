e5e898b746c8f742d765c6df963b58a7e784bd21
Vitsky <vitskyds@gmail.com>
2018-05-06 20:19:34 +0300

    refac: refactored functions to ES6

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,13 +8,13 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function () {
+$(() => {
 
-    describe('RSS Feeds', function () {
+    describe('RSS Feeds', () => {
         /* Tests to make sure that the allFeeds variable has
          * been defined and that it is not empty.
          */
-        it('are defined', function () {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -23,9 +23,9 @@ $(function () {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('have a valid url', function () {
+        it('have a valid url', () => {
             let feedId = 0;
-            allFeeds.forEach(function (feed) {
+            allFeeds.forEach((feed) => {
                 feed.id = feedId;
                 expect(allFeeds[feedId].url).toBeDefined();
                 expect(allFeeds[feedId].url.length).toBeGreaterThan(0);
@@ -37,9 +37,9 @@ $(function () {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('have a valid name', function () {
+        it('have a valid name', () => {
             let feedId = 0;
-            allFeeds.forEach(function (feed) {
+            allFeeds.forEach((feed) => {
                 feed.id = feedId;
                 expect(typeof allFeeds[feedId].name === 'string').toBe(true);
                 expect(allFeeds[feedId].name.length).toBeGreaterThan(0);
@@ -49,20 +49,20 @@ $(function () {
     });
 
     /* Test suite that checks menu functionality */
-    describe('The menu', function () {
+    describe('The menu', () => {
         const body = document.getElementById('body');
 
         /* Test that ensures the menu element is
          * hidden by default.
          */
-        it('hides the menu by default', function () {
+        it('hides the menu by default', () => {
             expect(body).toHaveClass('menu-hidden');
         });
 
         /* Test that ensures the menu changes
          * visibility when the menu icon is clicked.
          */
-        it('visibility is toggled upon click on menu icon', function () {
+        it('visibility is toggled upon click on menu icon', () => {
 
             $('.menu-icon-link').trigger('click');
             expect($(body).hasClass('menu-hidden')).toBe(false);
@@ -74,29 +74,29 @@ $(function () {
 });
 
 /* Test suite that checks the loadFeed functionality*/
-describe('Initial Entries', function () {
+describe('Initial Entries', () => {
 
     /* Test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
      */
     let feedLength;
-    beforeEach(function (done) {
-        loadFeed(0, function () {
+    beforeEach((done) => {
+        loadFeed(0, () => {
             feedLength = $('.feed .entry').length;
             done();
         });
 
     });
 
-    it('has at least one entry', function (done) {
+    it('has at least one entry', (done) => {
         expect(feedLength).toBeGreaterThan(0);
         done();
     });
 });
 
 /* Test suite that checks the feed functionality*/
-describe('New Feed Selection', function () {
+describe('New Feed Selection', () => {
 
     /* Test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
@@ -104,19 +104,19 @@ describe('New Feed Selection', function () {
     let feedA,
         feedB;
 
-    beforeEach(function (done) {
-        loadFeed(0, function () {
+    beforeEach((done) => {
+        loadFeed(0, () => {
             feedA = document.querySelector('.feed').innerHTML;
         });
 
-        loadFeed(1, function () {
+        loadFeed(1, () => {
             feedB = document.querySelector('.feed').innerHTML;
             done();
         });
     });
 
     /* Check if feeds have been added to the feedList*/
-    it('loads new feeds', function (done) {
+    it('loads new feeds', (done) => {
         expect(feedB !== feedA ? true : false).toBe(true);
         done();
     });
